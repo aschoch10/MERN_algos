d0ce2eacd64c6015b722300a391077e21319a1f7
Andrew <aschoch10@gmail.com>
2022-01-19 10:17:31 -0800

    solved

--- intersection.js
+++ intersection.js
@@ -1,8 +1,8 @@
 /* 
-  Efficiently combine two already sorted multiset arrays 
-  into an array containing the sorted set intersection of the two.
-  Output: only the shared values between the two arrays (deduped).
-  Venn Diagram Visualization (bottom) https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
+Efficiently combine two already sorted multiset arrays 
+into an array containing the sorted set intersection of the two.
+Output: only the shared values between the two arrays (deduped).
+Venn Diagram Visualization (bottom) https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
 */
 
 const arrA1 = [0, 1, 2, 2, 2, 7];
@@ -31,4 +31,39 @@ const expected3 = [];
  *    sorted and contains only the shared values between the two arrays
  *    deduped.
  */
- function orderedIntersection(sortedA, sortedB) {}
+
+function orderedIntersection(sortedA, sortedB) {
+    //setting empty array to push common values to
+    const resultsArray = []
+    //counters to keep track of the indexes
+    let counterA = 0
+    let counterB = 0
+    //terminates after checking all the common elements in at least one of the arrays, because if it's not in one array we don't care if it's in the other or not
+    while (counterA < sortedA.length && counterB < sortedB.length) {
+        //if the two values are equal we know they are intersecting 
+        if (sortedA[counterA] == sortedB[counterB]) {
+            //check to see if it's already in the resultsArray that we created
+            if (resultsArray[resultsArray.length - 1] != sortedA[counterA]) {
+                // console.log (resultsArray[resultsArray.length - 1])
+                // console.log(sortedA[counterA]);
+                // and if it's not in the array then we simply add it in 
+                resultsArray.push(sortedB[counterB])
+            }
+            //bc they're both equal we increment them both
+            counterA++
+            counterB++
+        }
+        //if a < b increment a because we can assume they are sorted so we make one bigger until they are they same or larger
+        else if (sortedA[counterA] < sortedB[counterB]) {
+            counterA++
+            //the opposite of 56
+        } else {
+            counterB++
+        }
+    }
+    return resultsArray
+}
+
+console.log(orderedIntersection(arrA1, arrB1));
+console.log(orderedIntersection(arrA2, arrB2));
+console.log(orderedIntersection(arrA3, arrB3));
