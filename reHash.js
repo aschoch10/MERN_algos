2f084d59db83540a9d857b0e981f45301c1f756c
Andrew <aschoch10@gmail.com>
2022-02-01 11:23:41 -0800

    rehash

--- reHash.js
+++ reHash.js
@@ -1,10 +1,10 @@
 /*          __                        __         
-           /\ \                      /\ \        
- _ __    __\ \ \___      __      ____\ \ \___    
+/\ \                      /\ \        
+_ __    __\ \ \___      __      ____\ \ \___    
 /\`'__\/'__`\ \  _ `\  /'__`\   /',__\\ \  _ `\  
 \ \ \//\  __/\ \ \ \ \/\ \L\.\_/\__, `\\ \ \ \ \ 
- \ \_\\ \____\\ \_\ \_\ \__/.\_\/\____/ \ \_\ \_\
-  \/_/ \/____/ \/_/\/_/\/__/\/_/\/___/   \/_/\/_/
+\ \_\\ \____\\ \_\ \_\ \__/.\_\/\____/ \ \_\ \_\
+/_/ \/____/ \/_/\/_/\/__/\/_/\/___/   \/_/\/_/
     Given to a Coding Dojo alumni by Riot Games in 2021.
     Rehash an incorrectly hashed string by combining letter count occurrences
     and alphabetizing them.
@@ -14,7 +14,7 @@ const str1 = "b70a164c32a20c10";
 const expected1 = "a184b70c42";
 
 // step 1:
-frequencyMap = {}
+//frequencyMap = {}
 // step 2:
 // extract [keys? values? something?]
 // if it's an array -> sort
@@ -28,7 +28,20 @@ frequencyMap = {}
 // isNaN(x)
 // myObj.hasOwnProperty("key")
 
-function rehash(str) {}
+function rehash(str) {
+    let frequencyMap = {}
+    for (let i = 0; i < str.length; i++) {
+        if (isNaN(str[i]) ) {
+            console.log(str[i]);
+            if (frequencyMap.hasOwnProperty(str[i])) {
+                frequencyMap[str[i]] += str[i]
+
+            } else {
+                frequencyMap[str[i]] = str[i]
+            }
+        } 
+    }console.log(frequencyMap);
+}
 
 rehash(str1);
 console.log(rehash(str1) === expected1, "<-- should be \"true\"");
